1c1486e4e4829b1a77318ec58a9e5fd9eac8681c
Philipp Anders <philipp.anders.pa@gmail.com>
2016-05-29 02:01:33 +0200

    added fastfoodvanues.json

--- src/main/java/extract/FastFoodVenueExtractor.java
+++ src/main/java/extract/FastFoodVenueExtractor.java
@@ -34,45 +34,38 @@ public class FastFoodVenueExtractor {
 
 		while (it.hasNext()) {
 			String country = it.next();
+
+			Long id = 3600000000L + Long.parseLong(new OverpassApiService().getIdOfCountry(country));
+
 			System.out.println(country);
-			System.out.println(new OverpassApiService().getIdOfCountry(country));
+			System.out.println(id);
+
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet(
+					"http://api.openstreetmap.fr/oapi/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A200%5D%3Barea%28"
+							+ id
+							+ "%29%2D%3E%2EsearchArea%3B%28node%5B%22amenity%22%3D%22fast%5Ffood%22%5D%28area%2EsearchArea%29%3B%29%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B%0A");
+			HttpResponse response;
+			response = client.execute(request);
+			Scanner scanner = new Scanner(response.getEntity().getContent());
+			StringBuilder builder = new StringBuilder();
+			while (scanner.hasNext()) {
+				builder.append(scanner.nextLine());
+			}
+			JSONObject jObject = new JSONObject(builder.toString().trim());
+			JSONArray array = jObject.getJSONArray("elements");
+
+			for (int i = 0; i < array.length(); i++) {
+				JSONObject node = (JSONObject) array.get(i);
+				JSONObject tags = node.getJSONObject("tags");
+				String name = tags.has("name") ? tags.getString("name") : "unknown";
+				fastFoodVenues.add(new FastFoodVenue(country, name));
+			}
 		}
-
-//		 String country = it.next();
-//		 country =  it.next();
-//		 country = it.next();
-//		 country = it.next();
-//		 Long id = 3600000000L + Long.parseLong(new OverpassApiService().getIdOfCountry(country));
-//		 System.out.println(country);
-//		 System.out.println(id);
-//		 HttpClient client = new DefaultHttpClient();
-//		 HttpGet request = new HttpGet(
-//		 "http://api.openstreetmap.fr/oapi/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3Barea%28"
-//		 + id +
-//		 "%29%2D%3E%2EsearchArea%3B%28node%5B%22amenity%22%3D%22fast%5Ffood%22%5D%28area%2EsearchArea%29%3B%29%3Bout%20body%3B%3E%3Bout%20skel%20qt%3B%0A");
-//		 HttpResponse response;
-//		 response = client.execute(request);
-//		 Scanner scanner = new Scanner(response.getEntity().getContent());
-//		 StringBuilder builder = new StringBuilder();
-//		 while (scanner.hasNext()) {
-//		 builder.append(scanner.nextLine());
-//		 }
-//		 JSONObject jObject = new JSONObject(builder.toString().trim());
-//		 JSONArray array = jObject.getJSONArray("elements");
-//		
-//		 for (int i = 0; i < array.length(); i++) {
-//		 JSONObject node = (JSONObject) array.get(i);
-//		 JSONObject tags = node.getJSONObject("tags");
-//		 String name = tags.has("name") ? tags.getString("name") : "unknown";
-//		 fastFoodVenues.add(new FastFoodVenue(country, name));
-//		 }
-//		
-//		 Type typeOfSrc = new TypeToken<ArrayList<FastFoodVenue>>() {
-//		 }.getType();
-//		 Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//		 FileUtils.writeStringToFile(new File("json/fastFoodVenues.json"),
-//		 gson.toJson(fastFoodVenues, typeOfSrc));
-
+		Type typeOfSrc = new TypeToken<ArrayList<FastFoodVenue>>() {
+		}.getType();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		FileUtils.writeStringToFile(new File("json/fastFoodVenues.json"), gson.toJson(fastFoodVenues, typeOfSrc));
 	}
 
 }
